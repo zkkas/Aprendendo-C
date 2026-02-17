4a8f11645ee5345df4f388327b20bf2d9ac941d0
zkkas <gabrielpcgamer23@gmail.com>
2026-02-17 00:03:33 -0300

    aula2

---
+++ aula2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   int idade = 0; //variavel do tipo inteiro, com o nome idade, e valor inicial 0
+   printf("A idade inicial e %d \n", idade); //imprime a idade inicial, que e 0
+   printf("Informe uma idade: "); //imprime a mensagem "Informe uma idade: " para o usuario
+   scanf("%d", &idade); //le a idade informada pelo usuario e armazena na variavel idade, usando o formato %d para inteiro e o operador & para passar o endereco da variavel
+   printf("A idade informada foi %d \n", idade); //imprime a idade informada pelo usuario, usando o formato %d para inteiro e a variavel idade
+}
