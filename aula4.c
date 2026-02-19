d2d26a59f552f82db98ef97d8a2c7876f3990b39
zkkas <gabrielpcgamer23@gmail.com>
2026-02-18 21:27:45 -0300

    aula4.c

---
+++ aula4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int minhaidade = 20; //declara a variavel minhaidade do tipo inteiro e inicializa com o valor 20
+    int idadedopai = 40; //declara a variavel idadedopai do tipo inteiro e inicializa com o valor 40
+    int idadedamae = 41; //declara a variavel idadedamae do tipo inteiro e inicializa com o valor 41
+    
+    printf("A Minha idade e: %i \n a idade do meu pai e: %i \n e a idade da mãe e: %i", minhaidade, idadedopai, idadedamae ); //imprime as idades usando a funcao printf, onde %i é um placeholder para um inteiro e minhaidade, idadedopai e idadedamae sao as variaveis que armazenam as idades
+}
